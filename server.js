41f1ea22c0ce878482a71540a90055519c46161c
ed42311 <ed42311@gmail.com>
2018-02-06 11:22:29 -0700

    whatever message you wanted to add

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express()
+const port = process.env.PORT || 3001;
 
 app.use('/', express.static(path.join(__dirname, "public")));
 app.use('/trevor', express.static(path.join(__dirname, "public/trevor")));
@@ -13,4 +14,5 @@ app.use('/kelsey', express.static(path.join(__dirname, "public/kelsey")));
 app.use('/laura', express.static(path.join(__dirname, "public/laura")));
 app.use('/timfox', express.static(path.join(__dirname, "public/timfox")));
 app.use('/tyler', express.static(path.join(__dirname, "public/tyler")));
-app.listen(3001, () => console.log("it works 3001"))
+
+app.listen(port, () => console.log("it works 3001"))
