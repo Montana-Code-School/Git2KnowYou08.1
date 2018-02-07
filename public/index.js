160fb2fa1ce14a65e3e6dd2519be35770fdd119a
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-02-07 16:54:29 -0700

    update 752

--- public/index.js
+++ public/index.js
@@ -1,17 +1,26 @@
-var square = document.getElementById("square"),
-clickMe = document.getElementById('clickMe');
+var square = document.getElementsByClassName("square");
+for (var i = 0; i < square.length; i++) {
+  console.log(square[i]);
+  square[i].addEventListener("click", function(){
+    console.log("this works");
+    document.body.style.backgroundColor = "red";
+    document.getElementsByClassName('clickMe').style.backgroundColor = '#bdce00'
+  });
 
-function doDemo(){
-var button = this;
-square.style.backgroundColor = '#fa4';
-button.setAttribute("disabled", "true");
-setTimeout(clearDemo, 2000, button);
 }
-
-function clearDemo (button) {
-  var square = document.getElementById('square');
-  square.style.backgroundColor = "transparent";
-  button.removeAttribute("disabled");
-}
-
-clickMe.onclick = doDemo;
+//clickMe = document.getElementsByClassName('clickMe').style.background='#bdce00';
+console.log(square);
+// function doDemo(){
+// var button = this;
+// square.style.backgroundColor = '#bdce00';
+// button.setAttribute("disabled", "true");
+// setTimeout(clearDemo, 2000, button);
+// }
+//
+// function clearDemo (button) {
+//   var square = document.getElementsByClass('square');
+//   square.style.backgroundColor = "transparent";
+//   button.removeAttribute("disabled");
+// }
+//
+// clickMe.onclick = doDemo;
