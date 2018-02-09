73cae487d899803cf41e0a801a2507f8440ce275
Richards <timothyrichards@Timothys-MacBook-Pro.local>
2018-02-09 15:57:23 -0700

    deathclock

--- public/ben/index.js
+++ public/ben/index.js
@@ -12,7 +12,13 @@ dateButton.style.backgroundImage = "url ('http://cdn6.bigcommerce.com/s-8gyotsl3
 dateButton.style.width = "auto";
 document.body.appendChild(dateButton);
 function whyNot(){
-  alert(date);
+  var birthDate = new Date ('1996-03-02T12:30:30Z')
+  var age = date - birthDate;
+  var time = age / (1000 * 60 * 60 * 24) / 365;
+  var longTime = 140 * (1000 * 60 * 60 * 24);
+  var deathTime = longTime - time;
+
+  alert(deathTime);
 }
 dateButton.addEventListener('click', whyNot);
 console.log(date);
